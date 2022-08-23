b366fd6b2c11441acaab7cf78f7b1e64a3e3510c
Arvin Visi <arvinvisi03@gmail.com>
2022-08-23 13:39:26 +0430

    add the pssword_reset_email in registration

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
