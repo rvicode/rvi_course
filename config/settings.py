5e2a0037bcb9d2b01b0bc45e645dc62cf656ddbe
Arvin Visi <arvinvisi03@gmail.com>
2022-09-19 14:34:20 +0430

    add and edit admin template

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os.path
 
 # for environment variables
 env = Env()
@@ -139,11 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 # static file config
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('assets'))]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 # media file config
 MEDIA_URL = '/media/'
-MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
